f96d439f2b48acff69bea892ad56bf6e3e4b2661
VillafuerteJonathan <86273066+VillafuerteJonathan@users.noreply.github.com>
2026-02-25 21:20:39 -0500

    Actualización de configuración local para DB y env

--- src/config/db.js
+++ src/config/db.js
@@ -11,10 +11,11 @@ const pool = new Pool({
   database: process.env.DB_NAME,
   password: process.env.DB_PASSWORD,
   port: Number(process.env.DB_PORT),
+  ssl: { rejectUnauthorized: false }, // importante para Railway
 });
 
 pool.on('connect', () => {
-  console.log('✅ Conectado a la base de datos PostgreSQL en Railway');
+  console.log('Conectado a la base de datos PostgreSQL en Railway');
 });
 
 export default pool;
